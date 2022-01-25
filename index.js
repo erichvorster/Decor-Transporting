1aa57028fa02a59352c520bc2039ce442339ba45
erichvorster <erichvorster1@gmail.com>
2022-01-25 22:43:48 +0200

    6th commit

--- index.js
+++ index.js
@@ -1,27 +1,95 @@
-const navSlide = () =>{
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    burger.addEventListener('click', () =>{
-        nav.classList.toggle('nav-active');
-
-
-        navLinks.forEach((link,index) => {
-            if (link.style.animation) {
-                link.style.animation =''
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
-            }
-        })
-        
-    });
-
-    
-}
-
-navSlide();
-
 
 
 gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline({
+})
+
+tl.from(".hero-container ", {
+    duration: 1,
+    opacity:0,
+    ease: "power4",
+    y:20
+}, 1)
+
+
+tl.from(".anim1", {
+    duration: 1,
+    opacity:0,
+    ease: "power4",
+    y:20
+},'<0.1' )
+tl.from(".anim2", {
+    duration: 1,
+    opacity:0,
+    ease: "power4",
+    y:20
+},'<0.1' )
+tl.from(".anim3", {
+    duration: 1,
+    opacity:0,
+    ease: "power4",
+    y:20
+},'<0.1' )
+tl.from(".anim4", {
+    duration: 1,
+    opacity:0,
+    ease: "power4",
+    y:20
+},'<0.1' )
+
+
+// Body fade in animations 
+
+
+gsap.from(".box", {
+    scrollTrigger:".box",
+    y:25,
+    ease: "power4",
+    duration:1,
+    opacity:0,
+    delay:.5
+})
+
+gsap.from(".box1", {
+    scrollTrigger:".box1",
+    y:10,
+    ease: "power4",
+    duration:1,
+    opacity:0,
+    delay:.5
+})
+
+gsap.from(".box2", {
+    scrollTrigger:".box2",
+    y:10,
+    ease: "power4",
+    duration:1,
+    opacity:0,
+    delay:.5
+})
+
+gsap.from(".box3", {
+    scrollTrigger:".box3",
+    y:10,
+    ease: "power4",
+    duration:1,
+    opacity:0,
+    delay:.5
+})
+gsap.from(".box4", {
+    scrollTrigger:".box4",
+    y:10,
+    ease: "power4",
+    duration:1,
+    opacity:0,
+    delay:.5
+})
+gsap.from(".box5", {
+    scrollTrigger:".box5",
+    y:10,
+    ease: "power4",
+    duration:1,
+    opacity:0,
+    delay:.5
+})
